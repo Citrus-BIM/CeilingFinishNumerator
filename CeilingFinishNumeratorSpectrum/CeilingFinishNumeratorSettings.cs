e7b8b5bac3d8c5849bc8c7e8439f31ff534a7b4d
Евгений Мишарев <misharevea@gmail.com>
2026-05-26 12:59:18 +0300

    Учет стадий

---
+++ CeilingFinishNumeratorSpectrum/CeilingFinishNumeratorSettings.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace CeilingFinishNumerator
+{
+    public class CeilingFinishNumeratorSettings
+    {
+        public string CeilingFinishNumberingSelectedName { get; set; }
+        public bool ProcessSelectedLevel { get; set; }
+        public bool SeparatedBySections { get; set; }
+        public string SelectedLevelName { get; set; }
+        public string SelectedParameterName { get; set; }
+        public bool FillRoomBookParameters { get; set; }
+
+        private const string FileName = "CeilingFinishNumeratorSettings.xml";
+
+        private static string SettingsDirectory =>
+            Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                "Citrus BIM",
+                "CeilingFinishNumerator");
+
+        private static string SettingsFilePath => Path.Combine(SettingsDirectory, FileName);
+
+        public static CeilingFinishNumeratorSettings GetSettings()
+        {
+            if (!File.Exists(SettingsFilePath))
+                return new CeilingFinishNumeratorSettings();
+
+            try
+            {
+                using (var fs = new FileStream(SettingsFilePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+                {
+                    var xSer = new XmlSerializer(typeof(CeilingFinishNumeratorSettings));
+                    return xSer.Deserialize(fs) as CeilingFinishNumeratorSettings
+                           ?? new CeilingFinishNumeratorSettings();
+                }
+            }
+            catch
+            {
+                return new CeilingFinishNumeratorSettings();
+            }
+        }
+
+        public void SaveSettings()
+        {
+            Directory.CreateDirectory(SettingsDirectory);
+
+            var tmpPath = SettingsFilePath + ".tmp";
+
+            if (File.Exists(tmpPath))
+            {
+                try { File.Delete(tmpPath); } catch { }
+            }
+
+            var xSer = new XmlSerializer(typeof(CeilingFinishNumeratorSettings));
+            using (var fs = new FileStream(tmpPath, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                xSer.Serialize(fs, this);
+            }
+
+            TryReplaceOrMove(tmpPath, SettingsFilePath);
+        }
+
+        private static void TryReplaceOrMove(string tmpPath, string targetPath)
+        {
+            try
+            {
+                if (File.Exists(targetPath))
+                    File.Replace(tmpPath, targetPath, destinationBackupFileName: null);
+                else
+                    File.Move(tmpPath, targetPath);
+            }
+            catch
+            {
+                try
+                {
+                    if (File.Exists(targetPath))
+                        File.Delete(targetPath);
+
+                    File.Move(tmpPath, targetPath);
+                }
+                catch
+                {
+                }
+            }
+        }
+    }
+}
---
+++ CeilingFinishNumeratorSpectrum/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("CeilingFinishNumeratorSpectrum")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CeilingFinishNumeratorSpectrum")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("9792b3e7-0b0b-4874-a2fc-4daff57d0a2e")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
